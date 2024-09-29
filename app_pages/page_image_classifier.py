9973d49ff29d90d6a0429c57f39b6cfd19cddf9f
Vasilika Schnitzer <vasilika.schnitzer@gmail.com>
2024-09-30 00:57:25 +0200

    Try to find a valid tflite version

--- app_pages/page_image_classifier.py
+++ app_pages/page_image_classifier.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import tflite_runtime.interpreter as tflite
+import ailia_tflite as tflite
 #import keras
 #import tensorflow as tf
 #from tensorflow.keras.models import load_model
